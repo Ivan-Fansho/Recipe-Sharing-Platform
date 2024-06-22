5033eae4e26f8947fb02b0949f5467c81904928c
Ivan <ivanaleksandrov98@gmail.com>
2024-06-22 15:15:09 +0300

    adding fixed authentication to use cookies

--- backend/app/api/routes/users/router.py
+++ backend/app/api/routes/users/router.py
@@ -1,7 +1,16 @@
-import os
-from fastapi import APIRouter, Request, HTTPException, status, Depends
-from fastapi.responses import RedirectResponse, JSONResponse
+from datetime import timedelta
 
+from fastapi import APIRouter, Request, HTTPException, status, Depends, Body, Response
+from fastapi.responses import RedirectResponse, JSONResponse
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+from starlette import schemas
+from backend.app.api.authentication.authentication_service import ACCESS_TOKEN_EXPIRE_MINUTES, create_token, \
+    authenticate_user, get_current_user
+from backend.app.api.routes.users import service
+from backend.app.api.routes.users.dtos import UserViewDTO, UserDTO
+
+from backend.app.core.db_dependency import get_db
 
 user_router = APIRouter(prefix="/users", tags=["Users"])
 
@@ -9,5 +18,48 @@ user_router = APIRouter(prefix="/users", tags=["Users"])
 
 
 
+@user_router.post("/register")
+async def register_user(
+    user: UserDTO = Body(..., example={
+        "username": "fansho",
+        "password": "Password1!!",
+        "email": "ivanaleksandrov98@gmail.com"
+    }),
+    db: Session = Depends(get_db)
+):
+    created_user = service.create(user, db)
+    return {"message": f"User {created_user.username} created successfully."}
 
 
+@user_router.post("/login")
+def login(
+    response: Response,
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db)
+):
+    user = authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_token(data={"sub": user.username}, expires_delta=access_token_expires)
+
+    # Set the token in a secure, HttpOnly cookie
+    response.set_cookie(
+        key="access_token",
+        value=access_token,
+        httponly=True,
+        secure=True,  # Use True in production to ensure cookies are only sent over HTTPS
+        samesite="lax"  # Adjust as needed for your use case
+    )
+
+    return {"message": "Login successful"}
+
+@user_router.get("/me")
+def read_users_me(current_user: UserViewDTO = Depends(get_current_user)):
+    return current_user
+
+@user_router.post("/logout")
+def logout(response: Response):
+    response.delete_cookie(key="access_token")
+    return {"message": "Logout successful"}
--- backend/app/api/routes/users/service.py
+++ backend/app/api/routes/users/service.py
@@ -1,6 +1,5 @@
 import logging
 from sqlalchemy.orm import Session
-
 from backend.app.core.db_dependency import get_db
 from backend.app.core.models import User
 from .dtos import UserDTO, UpdateUserDTO, UserShowDTO, UserFromSearchDTO, ContactDTO
@@ -11,55 +10,55 @@ from mailjet_rest import Client
 from ...authentication.authentication_service import hash_pass
 
 logger = logging.getLogger(__name__)
-def registration_email_sender(user):
-    api_key = 'cdcb4ffb9ac758e8750f5cf5bf07ac9f'
-    api_secret = '8ec6183bbee615d0d62b2c72bee814c4'
-    mailjet = Client(auth=(api_key, api_secret), version='v3.1')
-    data = {
-        'Messages': [
-            {
-                "From": {
-                    "Email": "kis.team.telerik@gmail.com",
-                    "Name": "MyPyWallet"
-                },
-                "To": [
-                    {
-                        "Email": f"{user.email}",
-                        "Name": f"{user.fullname}"
-                    }
-                ],
-                "Subject": f"Registration to PyMyWallet",
-                "HTMLPart": f"<h3>Thanks for registering, please wait for your registration to be confirmed.</h3><br />May the delivery force be with you!",
-                "CustomID": f"UserID: {user.id}"
-            }
-        ]
-    }
-    mailjet.send.create(data=data)
-
-def email_sender(user):
-    api_key = 'cdcb4ffb9ac758e8750f5cf5bf07ac9f'
-    api_secret = '8ec6183bbee615d0d62b2c72bee814c4'
-    mailjet = Client(auth=(api_key, api_secret), version='v3.1')
-    data = {
-        'Messages': [
-            {
-                "From": {
-                    "Email": "kis.team.telerik@gmail.com",
-                    "Name": "MyPyWallet Admin"
-                },
-                "To": [
-                    {
-                        "Email": "kis.team.telerik@gmail.com",
-                        "Name": "Kis"
-                    }
-                ],
-                "Subject": f"New Registration UserID:{user.id}",
-                "HTMLPart": f"<h3>New user {user.username} with id:{user.id} waits for confirmation</h3><br />May the delivery force be with you!",
-                "CustomID": "AppGettingStartedTest"
-            }
-        ]
-    }
-    mailjet.send.create(data=data)
+# def registration_email_sender(user):
+#     api_key = 'cdcb4ffb9ac758e8750f5cf5bf07ac9f'
+#     api_secret = '8ec6183bbee615d0d62b2c72bee814c4'
+#     mailjet = Client(auth=(api_key, api_secret), version='v3.1')
+#     data = {
+#         'Messages': [
+#             {
+#                 "From": {
+#                     "Email": "kis.team.telerik@gmail.com",
+#                     "Name": "MyPyWallet"
+#                 },
+#                 "To": [
+#                     {
+#                         "Email": f"{user.email}",
+#                         "Name": f"{user.fullname}"
+#                     }
+#                 ],
+#                 "Subject": f"Registration to PyMyWallet",
+#                 "HTMLPart": f"<h3>Thanks for registering, please wait for your registration to be confirmed.</h3><br />May the delivery force be with you!",
+#                 "CustomID": f"UserID: {user.id}"
+#             }
+#         ]
+#     }
+#     mailjet.send.create(data=data)
+#
+# def email_sender(user):
+#     api_key = 'cdcb4ffb9ac758e8750f5cf5bf07ac9f'
+#     api_secret = '8ec6183bbee615d0d62b2c72bee814c4'
+#     mailjet = Client(auth=(api_key, api_secret), version='v3.1')
+#     data = {
+#         'Messages': [
+#             {
+#                 "From": {
+#                     "Email": "kis.team.telerik@gmail.com",
+#                     "Name": "MyPyWallet Admin"
+#                 },
+#                 "To": [
+#                     {
+#                         "Email": "kis.team.telerik@gmail.com",
+#                         "Name": "Kis"
+#                     }
+#                 ],
+#                 "Subject": f"New Registration UserID:{user.id}",
+#                 "HTMLPart": f"<h3>New user {user.username} with id:{user.id} waits for confirmation</h3><br />May the delivery force be with you!",
+#                 "CustomID": "AppGettingStartedTest"
+#             }
+#         ]
+#     }
+#     mailjet.send.create(data=data)
 
 
 
@@ -69,17 +68,14 @@ def create(user: UserDTO, db: Session):
 
         new_user = User(
             username=user.username,
-            password=hashed_password,
             email=user.email,
-            phone_number=user.phone_number,
-            fullname=user.fullname
+            password=hashed_password,
         )
         db.add(new_user)
         db.commit()
         db.refresh(new_user)
-        db.commit()
-        email_sender(new_user)
-        registration_email_sender(new_user)
+        # email_sender(new_user)
+        # registration_email_sender(new_user)
         return new_user
     except IntegrityError as e:
         logger.error(f"Integrity error during user creation: {e}")
@@ -99,7 +95,7 @@ def create(user: UserDTO, db: Session):
                 status_code=400, detail="Could not complete registration"
             ) from e
     except Exception as e:
-        db.rollback()
+        # db.rollback()
         logger.error(f"Unexpected error during user creation: {e}")
         raise HTTPException(
             status_code=500, detail="Internal server error"
