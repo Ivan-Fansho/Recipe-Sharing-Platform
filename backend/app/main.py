7ce21c67c317ba897975597670146c9c42f208e2
Ivan <ivanaleksandrov98@gmail.com>
2024-07-01 10:56:11 +0300

    adding admin population to main file

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from dotenv import load_dotenv
 from fastapi import FastAPI
@@ -6,12 +8,25 @@ from fastapi.openapi.utils import get_openapi
 from api.routes.users.router import user_router
 from backend.app.core import models
 from backend.app.core.database import engine
+from backend.app.core.db_dependency import get_db
+from backend.app.core.db_population import initialize_special_accounts
 from backend.app.core.models import Base
 import os
 from fastapi.openapi.docs import get_swagger_ui_html
 
-load_dotenv()
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Initialize special accounts and categories during application startup
+    db_gen = get_db()
+    db = next(db_gen)
+    try:
+        initialize_special_accounts(db)
+    finally:
+        db_gen.close()
+
+    yield  # Control is passed to the application
+
+app = FastAPI(lifespan=lifespan)
 
 
 from api.routes.users.router import user_router
